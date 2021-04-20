812310b81c7e3c2ef81adddd1c081ce6d7ffd78f
Fatima Turgunbaeva <fturgunbaeva@gmail.com>
2021-04-20 16:35:59 +0600

    33

--- JS/33-сабак/script/add_to_cart.js
+++ JS/33-сабак/script/add_to_cart.js
@@ -1,11 +1,12 @@
 var elements = document.getElementsByClassName("add_cart");
 
 var myFunction = function(evt) {
-	
 	let title = this.parentNode.parentNode.parentNode.querySelectorAll(".item__title a");
-	alert(title[0].innerHTML)
+
+	console.log(title[0]);
 };
 
 for (var i = 0; i < elements.length; i++) {
 	elements[i].addEventListener('click', myFunction, false);
-}
+}	
+
