e8a9bd1fca18fdcd727d0e2643c73f6111a4d928
Fatima Turgunbaeva <fturgunbaeva@gmail.com>
2021-04-21 12:50:24 +0600

    33-cabak

--- JS/33-сабак/script/add_to_cart.js
+++ JS/33-сабак/script/add_to_cart.js
@@ -1,9 +1,28 @@
 var elements = document.getElementsByClassName("add_cart");
 
-var myFunction = function(evt) {
+var myFunction = function() {
 	let title = this.parentNode.parentNode.parentNode.querySelectorAll(".item__title a");
-
-	console.log(title[0]);
+	let price = this.parentNode.parentNode.parentNode.querySelectorAll(".item__title .price span");
+	let img = this.parentNode.parentNode.parentNode.querySelectorAll(".item__img img");
+	let create_li = document.createElement('li');
+	create_li.classList.add('bag__item');
+	create_li.innerHTML = '<div class="bag__img">\
+								<a href="?search=#">\
+									<img src=" '+img[0].src +'" alt="">\
+								</a>\
+							</div>\
+							<div class="bag__info">\
+								<p class="info__title"><a href="?search=#">'+title[0].innerText+'</a></p>\
+								<div class="info__price">\
+									<span>'+price[0].innerText+'</span>\
+								</div>\
+								<div class="trash">\
+									<a href="?search=#"><i class="far fa-trash-alt"></i></a>\
+								</div>\
+							</div>';
+	let block_cart = document.querySelector('.shopcard__dropdown ul');
+	block_cart.appendChild(create_li);
+	alert("Товары успешно добавлены в корзину!");
 };
 
 for (var i = 0; i < elements.length; i++) {
